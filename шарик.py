be2223ee747465a83ace3576de9a5faa301350a5
harley029 <harley029@gmail.com>
2024-01-08 12:53:16 +0100

    first steps

---
+++ шарик.py
@@ -0,0 +1,44 @@
+import pygame
+from pygame.constants import QUIT
+pygame.init()
+
+HEIGHT = 700
+WIDTH = 1200
+COLOR_WHITE = (255,255,255)
+COLOR_BLACK = (0,0,0)
+FPS = pygame.time.Clock()
+
+main_display = pygame.display.set_mode((WIDTH, HEIGHT))
+
+player_size = (20,20)
+player = pygame.Surface(player_size)
+player.fill(COLOR_WHITE)
+player_rect = player.get_rect()
+player_speed = [1,1]
+
+playing = True
+while playing:
+    FPS.tick(300)
+   
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            playing = False
+
+    main_display.fill(COLOR_BLACK)
+
+    if player_rect.bottom >= HEIGHT:
+        player_speed[1] = -player_speed[1]
+   
+    if player_rect.right >= WIDTH:
+        player_speed[0] = -player_speed[0] 
+    
+    if player_rect.top < 0:
+        player_speed[1] = -player_speed[1] 
+
+    if player_rect.right < 0:
+        player_speed[0] = -player_speed[0]
+    
+
+    main_display.blit(player, player_rect)
+    player_rect = player_rect.move(player_speed)
+    pygame.display.flip()
